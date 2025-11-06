0f8cdc19668922d21697ca2b0acf87a5d2734eea
Snoopak <anatoliy1997@hotmail.com>
2025-11-06 19:06:44 +0200

    .

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,16 +11,6 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-// 🔥 ТИМЧАСОВО ВІДКЛЮЧЕНО Service Worker через конфлікт з CORS proxy
-// Після переходу на GitHub CDN можна увімкнути назад
-// serviceWorkerRegistration.register();
-
-// Відключаємо Service Worker
-serviceWorkerRegistration.unregister();
-
-console.log('✅ Service Worker відключено - імпорт за URL має працювати');
+console.log('🚀 Додаток запущено без Service Worker');
